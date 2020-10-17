8072503618ae1facf155d6f8fc4a63d1cca12c40
Frans Larsson <frans.larsson@gmail.com>
2020-10-17 16:03:40 +0200

    feat: Add linear regression implementation

---
+++ linear_regression.py
@@ -0,0 +1,34 @@
+
+import numpy as np
+
+
+class LinearRegression(object):
+    """Linear regression.
+
+    Parameters
+    ----------
+    intercept : bool
+        If True then the model will be fitted with an intercept.
+
+    Attributes
+    ----------
+    beta : ndarray
+      The beta coefficients. Will be of shape (m,) where m is the number of
+      features in the provided training data.
+    """
+
+    def __init__(self, intercept=True):
+        self.beta = None
+        self.intercept = intercept
+
+    def fit(self, X, y):
+        if self.intercept:
+            ones = np.ones(shape=(X.shape[0], 1))
+            X = np.hstack([ones, X])
+        self.beta = np.linalg.pinv((X.T.dot(X))).dot(X.T).dot(y).reshape(-1, 1)
+
+    def predict(self, X):
+        if self.intercept:
+            ones = np.ones(shape=(X.shape[0], 1))
+            X = np.hstack([ones, X])
+        return X.dot(self.beta)
