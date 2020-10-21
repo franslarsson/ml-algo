9211ab0f4a92d838f0138a39521bb5c1f85b46d4
Frans Larsson <frans.larsson@gmail.com>
2020-10-21 21:35:31 +0200

    feat: Add methods to calculate and update posterior

--- bayesian_regression.py
+++ bayesian_regression.py
@@ -15,3 +15,12 @@ class BayesianLinearRegression(object):
 
     def predict(self):
         pass
+    
+    def calc_posterior(self, X):
+        s_prior_inv = np.linalg.pinv(self.w_prior.cov)
+        s_post = np.linalg.pinv(s_prior_inv + X.T.dot(X) / self.s)
+        mu_post = s_post.dot(s_prior_inv.dot(self.w_prior.mean) + X.T.dot(y) / self.s)
+        return multivariate_normal(mean=mu_post, cov=s_post)
+
+    def update_prior(self, posterior):
+        self.w_prior = posterior
